f851a02335ca1038e0a27ba02ac1162e56e04f06
Ellnarka <258500@edu.p.lodz.pl>
2026-06-07 23:36:37 +0200

    17. dodanie dokumentacji

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,25 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Implementation of a counting-out rhymer that follows
+ * the FIFO (First In First Out) principle.
+ * Elements are returned in the same order
+ * in which they were inserted.
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * Temporary storage used during FIFO operations.
+     */
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Removes and returns the oldest element stored in the rhymer.
+     * The method rearranges elements using a temporary rhymer
+     * to achieve FIFO behavior.
+     *
+     * @return the first inserted element
+     */
     @Override
     public int countOut() {
         while (!callCheck())
@@ -17,6 +33,12 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
         return ret;
     }
 
+    /**
+     * Returns the temporary rhymer used internally
+     * for FIFO operations.
+     *
+     * @return temporary DefaultCountingOutRhymer instance
+     */
     public DefaultCountingOutRhymer getTemp() {
         return temp;
     }
