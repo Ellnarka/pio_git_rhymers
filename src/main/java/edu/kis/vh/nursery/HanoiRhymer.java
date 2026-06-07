944f4f77b9c0be2bd5ea87e68ac9e30fa76a7afe
Ellnarka <258500@edu.p.lodz.pl>
2026-06-07 22:46:47 +0200

    11. hermytyzacja i 9.1 poprawiona widocznosc

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -3,7 +3,7 @@ package edu.kis.vh.nursery;
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     public static final int ZERO = 0;
-    int totalRejected = ZERO;
+    private int totalRejected = ZERO;
 
     public int reportRejected() {
         return totalRejected;
@@ -14,5 +14,9 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
             totalRejected++;
         else
             super.countIn(in);
+    }
+
+    public void setTotalRejected(int totalRejected) {
+        this.totalRejected = totalRejected;
     }
 }
